8a4562e49ac0a351043bd1d920310dcd9ca02cb0
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-05-28 21:40:37 -0300

    Adicionando biblioteca de ícones e criando primeiro componente

---
+++ boutique-de-plantao/src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FiSearch } from "react-icons/fi";
+
+function Header() {
+    return (
+        <>
+        
+            <FiSearch />
+        </>
+    )
+}
+
+export default Header;

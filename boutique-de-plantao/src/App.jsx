09f69f8f1d4f46fc468cf59a747c52dd7df15b78
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-05-30 23:32:40 -0300

    Criando o componente do header e da navbar

--- boutique-de-plantao/src/App.jsx
+++ boutique-de-plantao/src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
+// import './App.css'
 import Header from './components/header'
+import Navbar from './components/navbar'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Navbar/>
     </>
   )
 }
--- boutique-de-plantao/src/components/header.jsx
+++ boutique-de-plantao/src/components/header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import { FiSearch } from "react-icons/fi";
+import {BsPersonCircle} from "react-icons/Bs"
+import {AiOutlineShoppingCart} from "react-icons/Ai"
+import "../styles/header.css"
 
 function Header() {
     return (
-        <>
+       <header>
+        <div id="header-top">
+                <a href="./"><img src="../images/logos/logo_completo.png" alt="Logo da empresa" id="header-logo"/></a>
         
-            <FiSearch />
-        </>
+                
+                <div id="container-search">
+                    <input type="text" name="input" id="input-search"/>
+                <button> <FiSearch /></button>
+                </div>
+                
+                <div id="header-buttons">
+                    <button className="header-button"> <AiOutlineShoppingCart className="header-icon"/></button>
+                    <button className="header-button"> <BsPersonCircle className="header-icon" /></button>
+                </div>
+            </div>
+       </header>
     )
 }
 

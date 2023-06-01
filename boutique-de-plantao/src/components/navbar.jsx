7d4054643b1ac4c61d0f6681e0a8f81b0bcd201b
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-05-31 22:18:38 -0300

    Adicionando responsividade ao header

--- boutique-de-plantao/src/components/navbar.jsx
+++ boutique-de-plantao/src/components/navbar.jsx
@@ -4,15 +4,15 @@ import "../styles/navbar.css"
 function Navbar() {
     return (
         <nav>
-                <ul id="navbar-list">
-                    <a href="#"><li>Plantas</li></a>
-                    <a href="#"><li>Flores</li></a>
-                    <a href="#"><li>Vasos</li></a>
-                    <a href="#"><li>Suportes</li></a>
-                    <a href="#"><li>Jardinagem</li></a>
-                    <a href="sobre.html"><li>Sobre Nós</li></a>
-                </ul>
-            </nav>
+            <ul id="navbar-list">
+                <a href="#"><li>Plantas</li></a>
+                <a href="#"><li>Flores</li></a>
+                <a href="#"><li>Vasos</li></a>
+                <a href="#"><li>Suportes</li></a>
+                <a href="#"><li>Jardinagem</li></a>
+                <a href="sobre.html"><li>Sobre Nós</li></a>
+            </ul>
+        </nav>
     )
 }
 

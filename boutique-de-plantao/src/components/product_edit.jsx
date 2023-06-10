a52dbc6fa30962be723a116e19da5dfe10d79635
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-06-10 19:23:37 -0300

    Terminando de adicionar o CRUD do adm

--- boutique-de-plantao/src/components/product_edit.jsx
+++ boutique-de-plantao/src/components/product_edit.jsx
@@ -1,64 +1,62 @@
 import React, { useState } from "react";
 
 function Edit_product(props) {
-  const [nomeProduto, setNomeProduto] = useState(props.dados.nome);
-  const [quantidade, setQuantidade] = useState(props.dados.quantidade);
-  const [valor, setValor] = useState(props.dados.valor);
+    const [nomeProduto, setNomeProduto] = useState(props.dados.nome);
+    const [quantidade, setQuantidade] = useState(props.dados.quantidade);
+    const [valor, setValor] = useState(props.dados.valor);
 
-  const handleNomeProdutoChange = (event) => {
-    setNomeProduto(event.target.value);
-  };
+    const handleNomeProdutoChange = (event) => {
+        setNomeProduto(event.target.value);
+    };
 
-  const handleQuantidadeChange = (event) => {
-    setQuantidade(event.target.value);
-  };
+    const handleQuantidadeChange = (event) => {
+        setQuantidade(event.target.value);
+    };
 
-  const handleValorChange = (event) => {
-    setValor(event.target.value);
-  };
+    const handleValorChange = (event) => {
+        setValor(event.target.value);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Aqui você pode fazer algo com os valores atualizados
-    console.log("Nome do Produto:", nomeProduto);
-    console.log("Quantidade:", quantidade);
-    console.log("Valor:", valor);
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Aqui você pode fazer algo com os valores atualizados
+        console.log("Nome do Produto:", nomeProduto);
+        console.log("Quantidade:", quantidade);
+        console.log("Valor:", valor);
+    };
 
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="nome-produto">Nome do Produto</label>
-        <input
-          type="text"
-          id="nome-produto"
-          value={nomeProduto}
-          onChange={handleNomeProdutoChange}
-        />
+    return (
+        <>
+            <img src={props.dados.imagem} alt="Imagem do Produto" />
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="nome-produto">Nome do Produto</label>
+                <input
+                    type="text"
+                    id="nome-produto"
+                    value={nomeProduto}
+                    onChange={handleNomeProdutoChange}
+                />
 
-        <label htmlFor="quantidade">Quantidade</label>
-        <input
-          type="text"
-          id="quantidade"
-          value={quantidade}
-          onChange={handleQuantidadeChange}
-        />
+                <label htmlFor="quantidade">Quantidade</label>
+                <input
+                    type="number"
+                    id="quantidade"
+                    value={quantidade}
+                    onChange={handleQuantidadeChange}
+                />
 
-        <label htmlFor="valor">Valor</label>
-        <input
-          type="text"
-          id="valor"
-          value={valor}
-          onChange={handleValorChange}
-        />
+                <label htmlFor="valor">Valor</label>
+                <input
+                    type="text"
+                    id="valor"
+                    value={valor}
+                    onChange={handleValorChange}
+                />
 
-        <label htmlFor="imagem">Imagem</label>
-        <img src={props.imagem} alt="Imagem do Produto" />
-
-        <input type="submit" value="Salvar" id="button-cadastrar-adm" />
-      </form>
-    </>
-  );
+                <input type="submit" value="Salvar" id="button-cadastrar-adm" />
+            </form>
+        </>
+    );
 }
 
 export default Edit_product;

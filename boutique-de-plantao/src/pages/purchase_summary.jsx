3cbcab79f4daa04d7b57c867c731ab830cd61652
amatsuuu <andrematsumoto02@usp.br>
2023-06-11 23:03:16 -0300

    Adição sumary.css

--- boutique-de-plantao/src/pages/purchase_summary.jsx
+++ boutique-de-plantao/src/pages/purchase_summary.jsx
@@ -5,6 +5,7 @@ import Header from '../components/header';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import Item from '../components/cart_item';
+import "../styles/pages/summary.css"
 
 function Payment() {
     const [itens, setItens] = useState([]);
@@ -56,49 +57,57 @@ function Payment() {
                 <div id="summary">
                     <h1>Resumo da Compra</h1>
 
-                    <h2>Produtos</h2>
-                    {itens.length > 0 && (
-                        <>
-                            {itens.map((item, index) => (
-                                <Item
-                                    key={item.id}
-                                    nome={item.nome}
-                                    quantidade_disponivel={item.quantidade_disponivel}
-                                    valor={item.valor}
-                                    imagem={item.imagem}
-                                    quantidade_carrinho={item.quantidade_carrinho}
-                                />
-                            ))}
-                        </>
-                    )}
+                    <div className='secao'>
+                        <h2>Produtos</h2>
+                        {itens.length > 0 && (
+                            <>
+                                {itens.map((item, index) => (
+                                    <Item
+                                        key={item.id}
+                                        nome={item.nome}
+                                        quantidade_disponivel={item.quantidade_disponivel}
+                                        valor={item.valor}
+                                        imagem={item.imagem}
+                                        quantidade_carrinho={item.quantidade_carrinho}
+                                    />
+                                ))}
+                            </>
+                        )}
+                    </div>
+
+                    <div className='secao'>
+                        <h2>Endereço</h2>
+                        <div id="personal-data">
+                            <h3> {usuarioLogado.endereco}</h3>
+                            <p> {usuarioLogado.cep} - {usuarioLogado.bairro}</p>
+                            <p>{usuarioLogado.cidade} - {usuarioLogado.estado}</p>
+                            <p>{usuarioLogado.nome} - {usuarioLogado.telefone}</p>
+                        </div>
+                    </div>
                     
-                    <h2>Endereço</h2>
-                    <div id="personal-data">
-                        <h3> {usuarioLogado.endereco}</h3>
-                        <p> {usuarioLogado.cep} - {usuarioLogado.bairro}</p>
-                        <p>{usuarioLogado.cidade} - {usuarioLogado.estado}</p>
-                        <p>{usuarioLogado.nome} - {usuarioLogado.telefone}</p>
+                    <div className='secao'>
+                        <h2>Forma de Pagamento</h2>
+                        <div id="payment-method">
+                            Pix
+                        </div>
                     </div>
 
-                    <h2>Forma de Pagamento</h2>
-                    <div id="payment-method">
-                        AAAA
-                    </div>
-
-                    <h2>Confirme sua Compra</h2>
-                    <div id="verify-purchase">
-                    <div className='cost'>
-                                <div>SUBTOTAL</div>
-                                <div>R$ {subtotal.toFixed(2)}</div>
-                            </div>
-                            <div className='cost'>
-                                <div>FRETE</div>
-                                <div>R$ {frete.toFixed(2)}</div>
-                            </div>
-                            <div className='cost'>
-                                <div>TOTAL</div>
-                                <div>R$ {(subtotal + frete).toFixed(2)}</div>
-                            </div>
+                    <div className='secao' id='confirmation'>
+                        <h2>Confirme sua Compra</h2>
+                        <div id="verify-purchase">
+                        <div className='cost'>
+                                    <div>SUBTOTAL</div>
+                                    <div>R$ {subtotal.toFixed(2)}</div>
+                                </div>
+                                <div className='cost'>
+                                    <div>FRETE</div>
+                                    <div>R$ {frete.toFixed(2)}</div>
+                                </div>
+                                <div className='cost'>
+                                    <div>TOTAL</div>
+                                    <div>R$ {(subtotal + frete).toFixed(2)}</div>
+                                </div>
+                        </div>
                     </div>
                                 
                     <input type="submit" id="confirm-purchase" value="Confirmar" onClick={handlePurchase} />

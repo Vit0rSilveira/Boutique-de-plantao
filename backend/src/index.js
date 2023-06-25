bfd9a111c6e4abe4040650e08cb428685fd5f065
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-06-25 12:27:07 -0300

    Criando o CRUD do usuário

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config();
+const personRoutes = require('./routes/personRoutes')
 
-const app = express()
 const PORT = process.env.PORT
 const PASSWORD_MONGODB = process.env.PASSWORD_MONGODB
 const USER_MONGODB = process.env.USER_MONGODB
+
+const app = express()
 
 // Forma de ler JSON / middlwares
 app.use(
@@ -13,9 +15,11 @@ app.use(
 )
 
 app.use(express.json())
+app.use('/usuario', personRoutes)
+
 
 // Rota inicial
-app.get("/", (req, res) => {res.json({message: "Teste"}).status(200)})
+app.get("/", (req, res) => {res.json({message: "Oi andrezinho seu linduuu"}).status(200)})
 
 mongoose.connect(`mongodb+srv://${USER_MONGODB}:${PASSWORD_MONGODB}@boutique-de-plantao.0bhfoya.mongodb.net/?retryWrites=true&w=majority`)
 .then(() => {
---
+++ backend/src/models/Person.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const Person = mongoose.model('Person', {
+    nome : String,
+    tipo: String,
+    email: String,
+    senha: String,
+    tel: String,
+    endereco: String,
+    numero: Number,
+    cidade: String,
+    bairro: String,
+    estado: String,
+    complemento: String,
+})
+
+module.exports = Person

b895cd63b3160b4f0f5d0b92213a8f7d36c30356
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-06-26 20:51:03 -0300

    Adicionando imagens ao banco de dados

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json())
 app.use('/usuario', personRoutes)
 app.use('/produto', productRoutes)
-app.use('/uploads', express.static('uploads'));
+app.use('/imagens', express.static("public/imagens"))
 
 // Rota inicial
 app.get("/", (req, res) => {res.json({message: "Oi andrezinho seu linduuu"}).status(200)})
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,29 +1,23 @@
 const router = require('express').Router();
 const Product = require('../models/Product');
 const multer = require('multer');
-const storage = multer.memoryStorage(); // Armazenamento em memória para acessar os dados binários do arquivo
 
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 10 * 1024 * 1024, // Limite de tamanho do arquivo (10 MB)
-    },
-    fileFilter: (req, file, cb) => {
-        // Filtra os tipos de arquivo permitidos (imagens)
-        if (file.mimetype.startsWith('image/')) {
-            cb(null, true);
-        } else {
-            cb(new Error('Tipo de arquivo inválido. Apenas imagens são permitidas.'));
-        }
-    },
-});
 
-router.post('/', upload.single('imagem'), async (req, res) => {
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/imagens")
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname)
+    }
+
+})
+
+const upload = multer({storage});
+
+router.post('/', upload.single('file'), async (req, res) => {
     const { nome, codigo, quantidade_disponivel, descricao, valor } = req.body;
-    const imagem = {
-        data: req.file.buffer, // Obtém os dados binários da imagem a partir do arquivo enviado
-        contentType: req.file.mimetype // Obtém o tipo de conteúdo da imagem
-    };
+    const imagem = req.file.path
 
     try {
         const buscaDB = await Product.findOne({ codigo: codigo });
@@ -52,30 +46,12 @@ router.get('/', async (req, res) => {
     try {
         const products = await Product.find();
 
-        const modifiedProducts = products.map(product => {
-            const modifiedProduct = {
-                nome: product.nome,
-                codigo: product.codigo,
-                quantidade_disponivel: product.quantidade_disponivel,
-                descricao: product.descricao,
-                imagem: product.imagem,
-                valor: product.valor,
-            };
-
-            if (product.imagem && product.imagem.data) {
-                modifiedProduct.imagem = `http://localhost:3000/imagens/${product._id}`;
-            }
-
-            return modifiedProduct;
-        });
-
-        return res.status(200).json(modifiedProducts);
+        return res.status(200).json(products);
     } catch (error) {
         console.log(error);
         return res.status(500).json({ error: error.message });
     }
 });
-
 
 
 router.get("/:texto", async (req, res) => {

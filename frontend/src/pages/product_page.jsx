381cd91ca58dd338bc96c768ca88cabdf942ad88
Vit0rSilveira <vitor.silveira98@gmail.com>
2023-07-01 17:51:24 -0300

    arrumando a questão de pegar o produto no banco de dados e fazer os cards

--- frontend/src/pages/product_page.jsx
+++ frontend/src/pages/product_page.jsx
@@ -3,108 +3,67 @@ import { useParams } from "react-router-dom";
 import Header from "../components/header";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
-import ProductCard from '../components/product_card';
 import Complete_product from "../components/complete_product_card";
 import Review from "../components/review";
 import "../styles/pages/product.css";
 import { useCookies } from "react-cookie";
 import Edit_product from "../components/product_edit";
 
-function ProductPage() {
-  const { idProduto } = useParams();
+function Product() {
+  const { codProduto } = useParams();
   const [cookies] = useCookies(['credentials'])
-  const [produtos, setProdutos] = useState([]);
+  const [produto, setProduto] = useState([]); // Definir o estado inicial como um vetor vazio
 
   useEffect(() => {
-    fetch("../jsons/flores.json")
+    fetch(`http://localhost:3000/produto/codigo/${codProduto}`)
       .then((response) => response.json())
-      .then((data) => setProdutos(Object.values(data)))
+      .then((data) => setProduto(data)) // Atualizar o estado com os produtos retornados
       .catch((error) => console.log(error));
-  }, []);
+  }, [codProduto]);
 
   const tipoUsuarioLogado = () => {
     if (cookies.credentials)
       return cookies.credentials.tipo
   }
 
-  const produtoEncontrado = produtos.find(
-    (produto) => produto.codigo === idProduto
-  );
-
-  function shuffle(array, remove) {
-    const filteredArray = array.filter((produto) => produto.codigo !== remove);
-    const shuffledArray = [...filteredArray];
-
-    let currentIndex = shuffledArray.length;
-    let temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-
-      temporaryValue = shuffledArray[currentIndex];
-      shuffledArray[currentIndex] = shuffledArray[randomIndex];
-      shuffledArray[randomIndex] = temporaryValue;
-    }
-
-    return shuffledArray;
-  }
-
-
+  console.log(`produto:`, produto.imagem); // Exibir o vetor de produtos no console
   return (
     <>
       <Header />
       <Navbar />
 
       <main>
-        {produtoEncontrado ? (
+        {produto && produto.imagem ? (
           tipoUsuarioLogado() === 'adm' ? (
             <Edit_product
-              id={idProduto}
-              codigo={idProduto}
-              nome={produtoEncontrado.nome}
-              valor={produtoEncontrado.valor}
-              quantidade_disponivel={produtoEncontrado.quantidade_disponivel}
-              descricao={produtoEncontrado.descricao}
-              imagem={produtoEncontrado.imagem}
+              key={produto.id}
+              codigo={produto.codigo}
+              nome={produto.nome}
+              valor={produto.valor}
+              quantidade_disponivel={produto.quantidade_disponivel}
+              descricao={produto.descricao}
+              imagem={`http://localhost:3000/${produto.imagem.replace("public/", "")}`}
             />
           ) : (
             <Complete_product
-              id={idProduto}
-              codigo={idProduto}
-              nome={produtoEncontrado.nome}
-              valor={produtoEncontrado.valor}
-              quantidade_disponivel={produtoEncontrado.quantidade_disponivel}
-              descricao={produtoEncontrado.descricao}
-              imagem={produtoEncontrado.imagem}
+              key={produto.id}
+              codigo={produto.codigo}
+              nome={produto.nome}
+              valor={produto.valor}
+              quantidade_disponivel={produto.quantidade_disponivel}
+              descricao={produto.descricao}
+              imagem={`http://localhost:3000/${produto.imagem.replace("public/", "")}`}
             />
           )
         ) : (
           <p>Carregando...</p>
         )}
-
-        {produtos.length > 3 && (
-          <div id="avaliacao">
-            <h2>Você também vai gostar de</h2>
-            <div id='recomendacoes'>
-              {shuffle(produtos, idProduto).slice(0, 3).map((produtoo) => (
-                <ProductCard
-                  codigo={produtoo.codigo}
-                  nome={produtoo.nome}
-                  quantidade_disponivel={produtoo.quantidade_disponivel}
-                  valor={produtoo.valor}
-                  descricao={produtoo.descricao}
-                  imagem={produtoo.imagem}
-                />
-              ))}
-            </div>
-          </div>
-        )}
       </main>
+
 
       <Footer />
     </>
   );
 }
 
-export default ProductPage;
+export default Product;
